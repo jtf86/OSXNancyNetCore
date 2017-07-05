c69690fee2313d12ce6c5fb5bc8f76658dcdf06a
John <john@epicodus.com>
2017-07-05 13:47:23 -0700

    remove Razor reference from Startup

--- Startup.cs
+++ Startup.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Razor;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Nancy.Owin;
