3a30adca38ed0c17df8fab520ff4e3ec5fa2b99a
John <john@epicodus.com>
2017-07-05 12:08:08 -0700

    get basic Nancy working

---
+++ Startup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Nancy;
+using Nancy.Owin;
+
+namespace coreNancy
+{
+    public class Startup
+    {
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app)
+        {
+
+            app.UseOwin(x => x.UseNancy());
+        }
+    }
+}
