1e0f112ae70a8e80f224c0787ebd3f0d6254fbc6
Staff <epicodus_staff@staff-5.local>
2017-07-06 15:54:35 -0700

    add methods

--- Objects/Dog.cs
+++ Objects/Dog.cs
@@ -1,16 +1,21 @@
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace coreNancy.Objects
 {
   public class Dog
   {
     public string _name;
+    public int _id;
     public static List<Dog> instances = new List<Dog> {};
 
-    public Dog(string newName)
+    public Dog(string newName, int id=0)
     {
         this._name = newName;
-        instances.Add(this);
+        // instances.Add(this);
     }
 
     public string GetName()
@@ -20,12 +25,60 @@ namespace coreNancy.Objects
 
     public static List<Dog> GetAll()
     {
-      return instances;
+      List<Dog> allDogs = new List<Dog>{};
+
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = new SqlCommand("SELECT * FROM dogs;", conn);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        int dogId = rdr.GetInt32(0);
+        string dogName = rdr.GetString(1);
+        Dog newDog = new Dog(dogName, dogId);
+        allDogs.Add(newDog);
+      }
+
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
+
+      return allDogs;
     }
 
     public void Save()
     {
-      
+      SqlConnection conn = DB.Connection();
+      conn.Open();
+
+      SqlCommand cmd = ("INSERT INTO dogs (name) OUTPUT INSERTED.id VALUES (@DogName);", conn);
+      SqlParameter name = new SqlParameter();
+      name.ParameterName = "@DogName";
+      name.Value = this._name;
+
+      cmd.Parameters.Add(name);
+      SqlDataReader rdr = cmd.ExecuteReader();
+
+      while(rdr.Read())
+      {
+        this._id = rdr.GetInt32(0);
+      }
+      if (rdr != null)
+      {
+        rdr.Close();
+      }
+      if (conn != null)
+      {
+        conn.Close();
+      }
     }
+
   }
 }
